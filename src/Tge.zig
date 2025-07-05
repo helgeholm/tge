d1f6689857b2c22f9c2a97883295f661f1ac2eec
Helge Holm <helge.holm@tv2.no>
2025-07-05 12:08:27 +0200

    Abstract out example Ball as game object

--- src/Tge.zig
+++ src/Tge.zig
@@ -2,63 +2,17 @@ const std = @import("std");
 const linux = std.os.linux;
 
 const Config = @import("Config.zig");
-const Sprite = @import("Sprite.zig");
 const Display = @import("Display.zig");
 const Object = @import("Object.zig");
 
 const stdin = std.io.getStdIn().reader();
-
-const Ball = struct {
-    sprite: Sprite = .{
-        .data = "" ++
-            " ,----, " ++
-            "| BALL |" ++
-            " '----' ",
-        .width = 8,
-    },
-    dx: i16 = 1,
-    dy: i16 = 1,
-    sw: usize,
-    sh: usize,
-    pub fn init(x: i16, y: i16, sw: usize, sh: usize) Ball {
-        var b = Ball{ .sw = sw, .sh = sh };
-        b.sprite.x = x;
-        b.sprite.y = y;
-        return b;
-    }
-    pub fn object(self: *Ball) Object {
-        return .{
-            .ptr = self,
-            .sprite = self.sprite,
-            .tick = Ball.tick,
-        };
-    }
-    pub fn tick(ptr: *anyopaque, keys: *[256]bool) void {
-        var self: *@This() = @ptrCast(@alignCast(ptr));
-        if (keys['x']) {
-            self.dx = -self.dx;
-            self.dy = -self.dy;
-        }
-        self.sprite.x += self.dx;
-        if (self.sprite.x < 0 or self.sprite.x >= self.sw - 7) {
-            self.dx = -self.dx;
-            self.sprite.x += 2 * self.dx;
-        }
-        self.sprite.y += self.dy;
-        if (self.sprite.y < 0 or self.sprite.y >= self.sh - 2) {
-            self.dy = -self.dy;
-            self.sprite.y += 2 * self.dy;
-        }
-    }
-};
 
 allocator: std.mem.Allocator,
 display: Display,
 keys: [256]bool = undefined,
 ticks: usize = 0,
 orig_termios: linux.termios,
-b: Ball,
-c: Ball,
+objects: std.ArrayList(Object),
 
 pub fn init(allocator: std.mem.Allocator, config: Config) !@This() {
     var termios: linux.termios = undefined;
@@ -73,12 +27,12 @@ pub fn init(allocator: std.mem.Allocator, config: Config) !@This() {
         .allocator = allocator,
         .orig_termios = termios,
         .display = try Display.init(allocator, config),
-        .b = Ball.init(0, 0, config.width, config.height),
-        .c = Ball.init(40, 27, config.width, config.height),
+        .objects = std.ArrayList(Object).init(allocator),
     };
 }
 
 pub fn deinit(self: *@This()) void {
+    self.objects.deinit();
     self.display.deinit(self.allocator);
     _ = linux.tcsetattr(0, linux.TCSA.NOW, &self.orig_termios);
 }
@@ -106,6 +60,11 @@ pub fn win_resized(self: *@This()) void {
     self.display.check_ready();
 }
 
+pub fn addObject(self: *@This(), o: Object) void {
+    const entry = self.objects.addOne() catch unreachable;
+    entry.* = o;
+}
+
 fn paused(self: @This()) bool {
     return (self.display.state == .unready);
 }
@@ -117,8 +76,9 @@ fn tick(self: *@This()) void {
         self.keys[stdin.readByte() catch break] = true;
     if (self.keys['0'])
         self.ticks = 0;
-    self.b.object().tick(self.b.object().ptr, &self.keys);
-    self.c.object().tick(self.b.object().ptr, &self.keys);
+    for (self.objects.items) |o| {
+        o.tick(o.ptr, &self.keys);
+    }
     self.ticks += 1;
 }
 
@@ -126,7 +86,10 @@ fn draw(self: *@This()) void {
     self.display.clear();
     const now = std.time.Instant.now() catch unreachable;
     self.display.print(10, 20, "{d},{d}.{d}", .{ self.ticks, now.timestamp.sec, now.timestamp.nsec });
-    self.display.blot(&self.b.object().sprite.?);
-    self.display.blot(&self.c.object().sprite.?);
+    for (self.objects.items) |o| {
+        if (o.sprite) |s| {
+            self.display.blot(s);
+        }
+    }
     self.display.draw();
 }
--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,66 @@
 const std = @import("std");
 const singleton = @import("singleton.zig");
+const Sprite = @import("Sprite.zig");
+const Object = @import("Object.zig");
+const Config = @import("Config.zig");
+
+const Ball = struct {
+    sprite: Sprite = .{
+        .data = "" ++
+            " ,----, " ++
+            "| BALL |" ++
+            " '----' ",
+        .width = 8,
+    },
+    dx: i16 = 1,
+    dy: i16 = 1,
+    sw: usize,
+    sh: usize,
+    pub fn init(x: i16, y: i16, sw: usize, sh: usize) Ball {
+        var b = Ball{ .sw = sw, .sh = sh };
+        b.sprite.x = x;
+        b.sprite.y = y;
+        return b;
+    }
+    pub fn object(self: *Ball) Object {
+        return .{
+            .ptr = self,
+            .sprite = &self.sprite,
+            .tick = Ball.tick,
+        };
+    }
+    pub fn tick(ptr: *anyopaque, keys: *[256]bool) void {
+        var self: *@This() = @ptrCast(@alignCast(ptr));
+        if (keys['x']) {
+            self.dx = -self.dx;
+            self.dy = -self.dy;
+        }
+        self.sprite.x += self.dx;
+        if (self.sprite.x < 0 or self.sprite.x >= self.sw - 7) {
+            self.dx = -self.dx;
+            self.sprite.x += 2 * self.dx;
+        }
+        self.sprite.y += self.dy;
+        if (self.sprite.y < 0 or self.sprite.y >= self.sh - 2) {
+            self.dy = -self.dy;
+            self.sprite.y += 2 * self.dy;
+        }
+    }
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak)
         std.log.err("Memory leak detected on exit", .{});
 
-    var t = try singleton.init(gpa.allocator(), .{ .width = 100, .height = 40 });
+    const config = Config{ .width = 100, .height = 40 };
+    var t = try singleton.init(gpa.allocator(), config);
     defer t.deinit();
+
+    var b = Ball.init(0, 0, config.width, config.height);
+    var c = Ball.init(40, 27, config.width, config.height);
+    t.addObject(b.object());
+    t.addObject(c.object());
 
     try t.run();
 }
