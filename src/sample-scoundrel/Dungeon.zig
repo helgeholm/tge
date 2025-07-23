2488dc16ef66de9e9e08b4ca3d497719b9d00ea7
Helge Holm <helge.holm@tv2.no>
2025-07-23 21:39:12 +0200

    Add dungeon size look variations

--- src/sample-scoundrel/Dungeon.zig
+++ src/sample-scoundrel/Dungeon.zig
@@ -3,14 +3,29 @@ const tge = @import("tge");
 
 const Deck = @import("Deck.zig");
 
+const imgFull: []const u8 = @embedFile("sprites/deck_full");
+const imgMedium: []const u8 = @embedFile("sprites/deck_medium");
+const imgSmall: []const u8 = @embedFile("sprites/deck_small");
+
 deck: *Deck,
 allocator: std.mem.Allocator,
 pile: std.ArrayList(*Deck.Card) = undefined,
-sprite: tge.Sprite = .{ .data = @embedFile("sprites/deck"), .width = 13 },
+sprite: tge.Sprite = .{ .data = imgFull, .width = 14 },
 
 pub fn draw(ptr: *anyopaque, display: *tge.Display) void {
     const self: *@This() = @ptrCast(@alignCast(ptr));
-    display.blot(&self.sprite, 20, 15);
+    if (self.pile.items.len > 30) {
+        self.sprite.data = imgFull;
+    } else if (self.pile.items.len > 2) {
+        self.sprite.data = imgMedium;
+    } else {
+        self.sprite.data = imgSmall;
+    }
+    if (self.pile.items.len > 0)
+        display.blot(&self.sprite, 20, 15);
+    var buf: [15]u8 = undefined;
+    const txt = std.fmt.bufPrint(&buf, "(({d} left))", .{self.pile.items.len}) catch undefined;
+    display.text(22, 26, txt);
 }
 
 pub fn init(self: *@This(), random: std.Random) void {
